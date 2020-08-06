8db147474fd7912cd8184dcb3a1ffa3f82cd9e1d
Shivam Goyal <shivamgoyal312@gmail.com>
2020-08-07 00:45:41 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/weather?address=Delhi').then((response) =>{
+fetch('/weather?address=Delhi').then((response) =>{
     response.json().then((data) =>{
         console.log(data)
     })
